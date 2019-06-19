70f0a85b455b6ea6c09cabae7ff193d9c7243e1a
Vo Tran Chi Hung <1753058@student.hcmus.edu.vn>
2019-06-19 12:01:37 +0700

    Add Lab09

---
+++ Lab09/01.cpp
@@ -0,0 +1,40 @@
+﻿/*
+HỌ TÊN: VÕ TRẦN CHÍ HUNG
+MSSV: 1753058
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+void split_covert(char *s);
+
+int main()
+{
+	char HOTEN[50];
+	char *s;
+	printf("Nhap ho va ten: ");
+	gets_s(HOTEN);
+	s = strupr(HOTEN);
+	printf("\n");
+	split_covert(s);
+	printf("\n");
+	getchar();
+	return 0;
+}
+
+void split_covert(char *s)
+{
+	int i, j, a;
+	j = 0;
+	a = 0;
+	if (s[0] != ' ')
+		printf("%c", s[0]);
+	for (i = 1; i < (strlen(s)); i++)
+	{
+		if (s[i - 1] == ' ' && s[i] != ' ')
+			printf("%c", s[i]);
+	}
+}
---
+++ Lab09/02.cpp
@@ -0,0 +1,70 @@
+﻿/*
+HỌ TÊN: VÕ TRẦN CHÍ HƯNG
+MSSV: 1753058
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+void encrypt_by_using_K(char A[100], int k);
+
+int main (void)
+{
+	char KYTU[100];
+	int k;
+	printf("Nhap chuoi ky tu can ma hoa: ");
+	gets_s(KYTU);
+	printf("Nhap khoa K: ");
+	scanf_s("%i", &k);
+	k = k % 26;
+	encrypt_by_using_K(KYTU, k);
+	printf("\n");
+	printf("Ky tu da ma hoa: ");
+	printf("\n");
+	puts(KYTU);
+}
+
+void encrypt_by_using_K(char A[100],int k)
+{
+	int new_code, a;
+	for (int i=0; A[i]!='\0'; i++)
+		{
+			if (A[i]>= 'a' && A[i]<='z')
+			{
+				new_code = A[i] + k;
+				if (new_code > 'z')
+				{
+					a = new_code - 'z' - 1;
+					A[i] = 'a' + a;
+				}
+				else
+					if (new_code < 'a')
+					{
+						a = 'a' - new_code - 1;
+						A[i] = 'z' - a;
+					}
+					else
+						A[i] = new_code;
+			}
+			else 
+				if (A[i]>='A'&& A[i]<= 'Z')
+				{
+					new_code = A[i] + k;
+					if (new_code > 'Z')
+					{
+						a = new_code - 'Z' - 1;
+						A[i] = 'A' + a;
+					}
+					else
+						if (new_code < 'A')
+						{
+							a = 'A' - new_code - 1;
+							A[i] = 'Z' - a;
+						}
+						else
+							A[i] = new_code;
+				}
+		}
+}
+
