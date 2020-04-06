6a2d6424950dc0e2b488240e18aae977d2338f77
Cy30mk3v <40456349+Cy30mk3v@users.noreply.github.com>
2020-04-06 23:24:16 +0700

    Update Ex_6.cpp

--- Lab02/Ex_6.cpp
+++ Lab02/Ex_6.cpp
@@ -4,8 +4,14 @@
 int main()
 {
 
-	int n1, n2, n3;
+	int n, n1, n2, n3;
 	printf("Nhap 3 chu so: ");
-	scanf("%i %i %i", &n1, &n2, &n3);
+	scanf("%i", &n);
+	//Số hàng trăm
+	n1=n/100;
+	//Số hàng chục
+	n2=(n-(n1*100))/10;
+    //Số hàng đơn vị
+	n3=(n-(n1*100)-(n2*10));
 	printf("%i\n", n1 + n2 + n3);
 }
