2d14b820bddd2c01a2c6e11626d03f6074935dbe
Cy30mk3v <40456349+Cy30mk3v@users.noreply.github.com>
2020-04-07 19:56:32 +0700

    Update 12.cpp

--- Lab05_Lab06/12.cpp
+++ Lab05_Lab06/12.cpp
@@ -11,28 +11,30 @@ int main(void)
 {
 	long int a;
 	printf("Nhap so nguyen (< 2 ty): ");
-	scanf_s("%li", &a);
+	scanf("%li", &a);
 	while (a > 2000000000)
 	{
 		printf("So nhap vao da sai, nhap so nguyen (< 2 ty):");
-		scanf_s("%li", &a);
+		scanf("%li", &a);
 	}
 	check_palindromic_number(a);
 }
 void check_palindromic_number(long int a)
 {
-	int rev, b, c, pn;
-	for (int i = 1; i < a; i++)
-	{
-		b = a;
-		rev = 0;
-		while (b > 0)
-		{
-			c = b % 10;
-			rev = 10 * rev + c;
-			b /= 10;
-		}
-		pn = (rev == a ? 1 : 0);
+	//Reverse, last digit, copy number
+	long int rev, l, copy;
+	int pn;
+	copy = a;
+	rev = 0;
+	while(copy){
+		//Get last digit of number
+		l = copy%10;
+		//Add to reverse
+		rev = rev *10 + l;
+		//The number remove last digit
+		copy/=10;
 	}
+	//Check if reverse and original is the same
+	pn = (rev == a)? 1:0;
 	printf("%i\n", pn);
 }
